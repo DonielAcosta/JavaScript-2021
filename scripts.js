5e63a214396c1026d6858ddf418545f2b44efc8c
donielacosta1995 <donielacosta1995@gmailcom>
2021-10-25 10:48:39 -0400

    operadores

---
+++ scripts.js
@@ -0,0 +1,30 @@
+// let numero = -5;
+
+// let palabra ='hola Mundo';
+// let respuesta = true
+// const PI = 3.14;
+// numero = 18;
+// console.log(respuesta);
+
+// operadores matematicos
+
+// let usuario ='Doniel'
+// let a= 10;
+// let b = 5;
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log(a%b);
+
+// console.log("hola" + usuario);
+
+// operadores incremento/decrementos
+
+let a = 10;
+
+// console.log(a);
+// console.log(a++);
+
+console.log(a--);
+console.log(a);
