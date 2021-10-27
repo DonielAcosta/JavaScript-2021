3e300e2797e70feb02192c3ce1187aea6ccc669c
donielacosta1995 <donielacosta1995@gmailcom>
2021-10-27 18:04:35 -0400

    k

--- 12  Bucles for of  for in/scripts.js
+++ 12  Bucles for of  for in/scripts.js
@@ -33,9 +33,9 @@ let names = ['Paco', 'José', 'Paula', 'María'];
 // }
 
 
-for( let i = 0; i < names.length; i++){
-    if(names[i]==='Paula'){
-        continue;
-    }
-    console.log(names[i]);
-}
+// for( let i = 0; i < names.length; i++){
+//     if(names[i]==='Paula'){
+//         continue;
+//     }
+//     console.log(names[i]);
+// }
