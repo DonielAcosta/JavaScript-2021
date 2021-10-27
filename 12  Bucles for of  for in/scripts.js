ff54a7a9d20510f299df76a70af6af699fa1031d
donielacosta1995 <donielacosta1995@gmailcom>
2021-10-27 16:27:00 -0400

    se realiza pruebas con el bucle for con of  e in el of obtienes los que contiene el array y el in te devuelve la posicion de dicho elemento

---
+++ 12  Bucles for of  for in/scripts.js
@@ -0,0 +1,41 @@
+/* Bucles */
+
+/* 
+    Determinados
+
+    bucle for of / for in
+
+    for(let variable of estructura){
+        Código a ejecutar
+    }
+
+    Palabras reservadas de bucles
+
+        break - Rompe el bucle
+        continue - Se salta la(s) posicion(es) que le indequemos y después continúa su ejecución
+*/
+
+let names = ['Paco', 'José', 'Paula', 'María'];
+
+// for(let name of names){
+//     console.log(name);  
+// }
+
+// for(let index in names){
+//     console.log(index);
+// }
+
+// for( let i = 0; i < names.length; i++){
+//     if(names[i]=='Paula'){
+//         break;
+//     }
+//     console.log(names[i]);
+// }
+
+
+for( let i = 0; i < names.length; i++){
+    if(names[i]==='Paula'){
+        continue;
+    }
+    console.log(names[i]);
+}
